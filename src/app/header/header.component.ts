0c1a19d457d7b2c870b1a96f65ddf9826f39a277
dani <danigomezlo2022@gmail.com>
2023-04-14 16:11:05 -0500

    HomeFinalizado

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+    
+  constructor () { }
 
+  ngOnInit(){
+     let btnMenu = document.getElementsByClassName('menu')[0]
+     let nav = document.getElementsByClassName('nav')[0]
+     
+     btnMenu.addEventListener('click', function(){
+      nav.classList.toggle('active')
+      btnMenu.classList.toggle('active')
+     })
+  }
 }
